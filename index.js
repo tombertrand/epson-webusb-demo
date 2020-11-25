ac3db542054015ed3c880a5eb281e42bace828cd
tombertrand <tom.bertrand@lightspeedhq.com>
2020-11-25 10:39:20 -0500

    Add "commands" for star/epson

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
 const usb = require("webusb").usb;
 
+fs.writeFileSync("epson-get-serial", Buffer.from([0x1d, 0x49, 68, 0x1b, 0x40]));
+fs.writeFileSync("epson-get-model", Buffer.from([0x1d, 0x49, 66]));
+fs.writeFileSync("star-get-model", Buffer.from([0x1b, 0x23, 0x2a, 0x0a, 0x00]));
+
 function selectEndpoint(direction, device) {
-  const endpoint = device.configuration.interfaces[0].alternate.endpoints.find(
-    (ep) => ep.direction === direction
-  );
+  const { endpoints } = device.configuration.interfaces[0].alternate;
+  console.log("~~~~endpoints", endpoints);
+  const endpoint = endpoints.find((ep) => ep.direction === direction);
 
   if (endpoint == null)
     throw new Error(`Endpoint ${direction} not found in device interface.`);
@@ -16,7 +20,7 @@ console.log("Searching for Web USB devices...");
   try {
     const device = await usb.requestDevice({
       filters: [
-        { vendorId: 0x04b8, productId: 0x0e20 }, // epson tmm30
+        // { vendorId: 0x04b8, productId: 0x0e20 }, // epson tmm30
         { vendorId: 0x0519, productId: 0x003 }, // star tsp100
       ],
     });
@@ -34,16 +38,27 @@ console.log("Searching for Web USB devices...");
       await device.claimInterface(0);
       console.log("Device claimed");
 
-      const endpoint = selectEndpoint("out", device);
+      const endpointOut = selectEndpoint("out", device);
+      const endpointIn = selectEndpoint("in", device);
 
-      const image =
-        device.manufacturerName === "STAR"
-          ? fs.readFileSync("./image-star")
-          : fs.readFileSync("./image-epson");
+      const command = fs.readFileSync("./star-get-model");
+
+      // const image =
+      //   device.manufacturerName === "STAR"
+      //     ? fs.readFileSync("./image-star")
+      //     : fs.readFileSync("./image-epson");
+
+      console.log("~~~command", command);
 
       console.log("Device transferOut");
-      device.transferOut(endpoint.endpointNumber, image);
+      console.log("~~~~endpointOut", endpointOut.endpointNumber);
+      device.transferOut(endpointOut.endpointNumber, command);
+      // device.transferOut(endpointOut.endpointNumber, image);
       console.log("Device transferOut completed");
+      console.log("~~~~endpointIn", endpointIn.endpointNumber);
+
+      const response = await device.transferIn(endpointIn.endpointNumber, 64);
+      console.log("~~~model response", response);
     } catch (err) {
       console.log("Printing error", err);
     }
